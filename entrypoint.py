7364c971ca3ad6d268b0e636bcff5b6ff2f239e4
saeideh-eslamian <s.eslamian.70@gmail.com>
2024-05-03 15:32:56 +0200

    fix: cleaner code for flake8 passed

--- entrypoint.py
+++ entrypoint.py
@@ -2,25 +2,30 @@ import json
 import redis as redis
 from flask import Flask, request
 from loguru import logger
-import requests
 
 
 HISTORY_LENGTH = 10
 DATA_KEY = "engine_temperature"
 
-# create a Flask server, and allow us to interact with it using the app variable
+# create a Flask server
 app = Flask(__name__)
 
 
 @app.route('/record', methods=['POST'])
 def record_engine_temperature():
+    """
+    recive engine_temperature and save it in the
+    engine_temperature_values if record request is successful
+    return 200
+    """
     payload = request.get_json(force=True)
     logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
 
     engine_temperature = payload.get("engine_temperature")
     logger.info(f"engine temperature to record is: {engine_temperature}")
 
-    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    database = redis.Redis(host="redis", port=6379,
+                           db=0, decode_responses=True)
     database.lpush(DATA_KEY, engine_temperature)
     logger.info(f"stashed engine temperature in redis: {engine_temperature}")
 
@@ -28,17 +33,23 @@ def record_engine_temperature():
         database.rpop(DATA_KEY)
     engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
     logger.info(
-        f"engine temperature list now contains these values: {engine_temperature_values}")
+        "engine temperature list now contains these values: "
+        f"{engine_temperature_values}"
+    )
 
-    logger.info(f"record request successful")
+    logger.info("record request successful")
     return {"success": True}, 200
-
 
 
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
+    """
+    calculate average engine temperature and
+    return average engine temperature and current engine temperature
+    """
 
-    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    database = redis.Redis(host="redis", port=6379,
+                           db=0, decode_responses=True)
 
     engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
 
@@ -50,6 +61,5 @@ def collect_engine_temperature():
 
     return {
         "current_engine_temperature": engine_temperature_values[0],
-        "average_engine_temperature" :average_engine_temperature
-        }, 200
-
+        "average_engine_temperature": average_engine_temperature
+    }, 200
